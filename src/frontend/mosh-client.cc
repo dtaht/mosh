0f7271d4577088764d76cfec166d41864bac1e51
Matthieu Boutier <boutier@pps.univ-paris-diderot.fr>
2014-09-17 11:11:52 +0200

    Make the waiting time for attaching gdb an option.

--- src/frontend/mosh-client.cc
+++ src/frontend/mosh-client.cc
@@ -103,11 +103,16 @@ int main( int argc, char *argv[] )
 
   /* Get arguments */
   int opt;
-  while ( (opt = getopt( argc, argv, "c" )) != -1 ) {
+  while ( (opt = getopt( argc, argv, "cw" )) != -1 ) {
     switch ( opt ) {
     case 'c':
       print_colorcount();
       exit( 0 );
+      break;
+    case 'w':
+      /* print PID and wait for a keystroke, to let time for gdb-attaching. */
+      printf("%d\n", getpid());
+      getchar();
       break;
     default:
       usage( argv[ 0 ] );
